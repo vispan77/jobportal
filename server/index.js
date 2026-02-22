24c0509ed94755b5567db6acc9bf47fe9f878af0
vispan77 <vishalpandey2332@gmail.com>
2026-02-22 16:18:13 +0530

    updated cors

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const cors = require("cors");
 
 app.use(
     cors({
-        origin: ["http://localhost:5173", "https://jobportal-six-liart.vercel.app"], 
+        origin: ["http://localhost:5173", "https://job-portal-neon-psi.vercel.app"], 
         credentials: true,
     })
 );
