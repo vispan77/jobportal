d951c5dd61bb36581bdd8561eb9b502338daa4ae
vispan77 <vishalpandey2332@gmail.com>
2026-02-22 16:39:33 +0530

    Update index.js
    updated and fix vercel issue

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const cors = require("cors");
 
 app.use(
     cors({
-        origin: ["http://localhost:5173", "https://job-portal-neon-psi.vercel.app"], 
+        origin: ["http://localhost:5173", "https://jobportal-olive-nine.vercel.app"], 
         credentials: true,
     })
 );
